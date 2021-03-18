c354211dfcea868dde590b24bd5fd1a6bdb7fbce
konstantin89 <email@gmail.com>
2021-03-18 22:52:53 +0000

    work in progress

--- example/main.c
+++ example/main.c
@@ -8,43 +8,52 @@
 
 
 
-#define FOO(RESULT) (RESULT = 2)
 
 #include <stddef.h>
-
 #include <stdio.h>
 
 
-//gcc 7.4.0
 
 
 
-#include  <stdio.h>
+#include <stdio.h>
 #include <stddef.h>
 
-#define container_of(ptr, type, member) ({ \
-         const typeof(((type *)0)->member) *__mptr = (ptr);    \
-         (type *)((char *)__mptr - offsetof(type,member)); })
 
-struct s
-{
-    int x;
-    int y;
-    int z;
+struct mystruct {
+    int data;
+    struct list_head *mylist;
 };
 
 
 int main(void)
 {
-    struct s str;
-    str.x = 1;
-    str.y = 2;
-    str.z = 3;
 
-    int *ptr = &str.x;
+    struct mystruct first = {
+     .data = 10,
+     .mylist = LIST_HEAD_INIT(first.mylist)
+    };
 
-    struct s* ptr2 = container_of(ptr, struct s, x);
+    LIST_HEAD(mylinkedlist);
 
-    printf("Hello, world! %d \n", ptr2->z);
+    list_add(&first.mylist, &mylinkedlist);
+
+
+    struct list_head *position = NULL;
+    struct mystruct  *datastructureptr = NULL;
+
+    list_for_each(position, &mylinkedlist)
+    {
+        datastructureptr = list_entry(position, struct mystruct, mylist);
+        printf("data  =  %d\n", datastructureptr->data);
+    }
+
+
+
+    //list_for_each_entry(datastructureptr, &mylinkedlist, mylist)
+    //{
+    //    printf("data  =  %d\n", datastructureptr->data);
+    //}
+
     return 0;
 }
